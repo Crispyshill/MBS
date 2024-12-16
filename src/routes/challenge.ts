6f81e025944d820acf6863996cd667a0caa66ffb
Seth Hillsberry <Shillsberry@protonmail.com>
2024-12-16 15:34:57 -0800

    Added endpoint to get one challenge by id

--- src/routes/challenge.ts
+++ src/routes/challenge.ts
@@ -20,4 +20,20 @@ router.get(
   }
 );
 
+
+router.get(
+  "/:challengeId",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const challengeId: string = req.params.challengeId; // Get the userId from the URL parameter
+
+      const result = await db.query("SELECT * FROM challenges WHERE id = $1;", [challengeId]);
+      const challenges: Challenge[] = result.rows; // Properly assign the rows to a typed variable
+      res.json(challenges); // Return the challenges array as the response
+    } catch (err) {
+      next(err); // Pass unhandled errors to the global error handler
+    }
+  }
+);
+
 export default router;
