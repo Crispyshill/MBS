3220fd352041961f9cebd86d1490384e6e1f69ee
Seth Hillsberry <Shillsberry@protonmail.com>
2024-12-16 10:38:26 -0800

    Updated CORS Policy

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import authRoutes from "./routes/auth";
 import * as dotenv from "dotenv";
+const cors = require("cors");
 dotenv.config();
 
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
+
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL, // Allow only this origin
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
+  credentials: true, // Allow cookies to be sent
+};
+
+// Enable CORS with specific options
+app.use(cors(corsOptions));
 
 // Middleware
 app.use(express.json());
